758915b446f9646de59173a8740c226aa8f46e2b
blandinelbb <linblandine@gmail.com>
2022-03-03 23:48:54 +0100

    modif position

--- front/src/Maps.js
+++ front/src/Maps.js
@@ -1,17 +1,15 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const position = [51.505, -0.09];
+const position = [48.86380957985594, 2.3443822975053807];
 function Maps() {
   return (
-    <MapContainer center={position} zoom={13}>
+    <MapContainer center={position} zoom={12}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={[48.86380957985594, 2.3443822975053807]}>
+        <Popup>Vous êtes ici</Popup>
       </Marker>
     </MapContainer>
   );
