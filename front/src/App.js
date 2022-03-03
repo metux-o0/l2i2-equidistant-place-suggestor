b1d9706f2b43fcad8f0a7b9b53e02e6238218d31
Rafika Sid <rafikaa.sid@gmail.com>
2022-03-03 14:37:49 +0100

    Ajout Carte.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,22 @@
-import Formulaire from './Formulaire';
-import Nav from './Nav';
-import Home from './Home';
-import Header from './Header';
-import Carte from './Carte';
+import Formulaire from "./Formulaire";
+import Nav from "./Nav";
+import Home from "./Home";
+import Header from "./Header";
+import Carte from "./Carte";
 
-import ReactDOM from 'react-dom';
-import { Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-
+import ReactDOM from "react-dom";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Nav></Nav>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/formulaire" element={<Formulaire />} />
-        <Route path='/carte' element={<Carte/>}/>
+        <Route path="/carte" element={<Carte />} />
       </Routes>
     </div>
   );
