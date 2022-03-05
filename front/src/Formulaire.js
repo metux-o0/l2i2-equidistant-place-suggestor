79369253b8e2d103dd93aff340f6892c4fdb62fa
blandinelbb <linblandine@gmail.com>
2022-03-05 21:16:13 +0100

    modification du fichier formulaire.js

--- front/src/Formulaire.js
+++ front/src/Formulaire.js
@@ -1,21 +1,49 @@
 import "./style/formulaire.css";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 function Formulaire() {
+  const [nom, setNom] = useState("");
+  const [adresse, setAdresse] = useState("");
+  const [dispo, setDispo] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/formulaire")
+      .then((res) => res.json())
+      .then((nom) => setNom(nom))
+      .then((adresse) => setAdresse(adresse));
+    //.then((dispo) => setDispo(dispo));
+  }, []);
+
+  function affiche() {
+    alert("Nom : " + nom + " Adresse : " + adresse);
+  }
+
   return (
-    <div className="form">
-      <h2>Formulaire</h2>
-      <form action="/log" method="post" id="formu">
-        <br />
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" required autofocus />
-        <br />
-        <label for="adresse">Adresse : </label>
-        <input type="text" name="adresse" id="adresse" required />
-        <br />
+    <div id="formulaire">
+      <form action="/log" method="post">
+        <label>
+          Nom :
+          <input
+            type="text"
+            value={nom}
+            onChange={(e) => {
+              setNom(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Adresse :
+          <input
+            type="text"
+            value={adresse}
+            onChange={(e) => {
+              setAdresse(e.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="Envoyer" onClick={affiche} />
+        <input type="button" value="Ajouter" />
       </form>
-      <br />
-      <Link to="/formulaire">Ajouter</Link>
     </div>
   );
 }
