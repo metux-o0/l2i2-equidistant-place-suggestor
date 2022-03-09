e28f917d9c6fc08831f99f256eda1de6e2b5470e
blandinelbb <linblandine@gmail.com>
2022-03-09 16:51:19 +0100

    modification du fichier formulaire

--- front/src/Formulaire.js
+++ front/src/Formulaire.js
@@ -2,48 +2,79 @@ import "./style/formulaire.css";
 import { useEffect, useState } from 'react';
 
 function Formulaire() {
-  const [nom, setNom] = useState("");
-  const [adresse, setAdresse] = useState("");
+  const [nom, setNom] = useState('');
+  const [adresse, setAdresse] = useState([]);
   const [dispo, setDispo] = useState([]);
 
+  var nbform = 1;
+
+  function getTd(ligne) {
+    var result =
+      '<div id="'+ligne+'"><label>Nom :</label><input type="text" onChange={(e) => {setNom(e.target.value);}}/><label>Adresse :</label><input type="text" onChange={(e) => {setAdresse(e.target.value);}}/><br />'+
+      '<label>Disponibilité :</label><select name="jour"multiple={true} size="7" onChange={(e) => {setDispo(e.target.value);}}><option value="Lundi">Lundi</option><option value="Mardi">Mardi</option><option value="Mercredi">Mercredi</option><option value="Jeudi">Jeudi</option><option value="Vendredi">Vendredi</option><option value="Samedi">Samedi</option><option value="Dimanche">Dimanche</option></select></div>';
+    return result;
+  }
+
+  function ajouterform() {
+    var nouvelem = document.createElement('form');
+    nouvelem.innerHTML = getTd(nbform++);
+
+    var dernelem = document.getElementsByTagName('form');
+    var parent = dernelem[dernelem.length-1];
+    parent.parentNode.insertBefore(nouvelem, parent);
+  }
   useEffect(() => {
-    fetch("http://localhost:3000/formulaire")
+    fetch('http://localhost:3000/formulaire')
       .then((res) => res.json())
       .then((nom) => setNom(nom))
-      .then((adresse) => setAdresse(adresse));
-    //.then((dispo) => setDispo(dispo));
+      .then((adresse) => setAdresse(adresse))
+      .then((dispo) => setDispo(dispo));
   }, []);
 
   function affiche() {
-    alert("Nom : " + nom + " Adresse : " + adresse);
+    alert(
+      'Nom : ' + nom + ' Adresse : ' + adresse + ' Disponibilité : ' + dispo
+    );
   }
 
   return (
     <div id="formulaire">
       <form action="/log" method="post">
-        <label>
-          Nom :
+        <div id="0">
+          <label>Nom :</label>
           <input
             type="text"
-            value={nom}
-            onChange={(e) => {
-              setNom(e.target.value);
-            }}
+            
           />
-        </label>
-        <label>
-          Adresse :
+          <label>Adresse :</label>
           <input
             type="text"
-            value={adresse}
             onChange={(e) => {
               setAdresse(e.target.value);
             }}
           />
-        </label>
-        <input type="submit" value="Envoyer" onClick={affiche} />
-        <input type="button" value="Ajouter" />
+          <br />
+          <label>Disponibilité :</label>
+          <select
+            name="jour"
+            multiple={true}
+            size="7"
+            onChange={(e) => {
+              setDispo(e.target.value);
+            }}
+          >
+            <option value="Lundi">Lundi</option>
+            <option value="Mardi">Mardi</option>
+            <option value="Mercredi">Mercredi</option>
+            <option value="Jeudi">Jeudi</option>
+            <option value="Vendredi">Vendredi</option>
+            <option value="Samedi">Samedi</option>
+            <option value="Dimanche">Dimanche</option>
+          </select>
+        </div>
       </form>
+      <input type="submit" value="Envoyer" onClick={affiche} />
+      <input type="button" value="Ajouter" onClick={ajouterform} />
     </div>
   );
 }
