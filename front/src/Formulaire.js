0f3c1a1bb5d2dbec5cc3ee038ae2107ffa49c51b
blandinelbb <linblandine@gmail.com>
2022-03-12 13:05:48 +0100

    modif formulaire

--- front/src/Formulaire.js
+++ front/src/Formulaire.js
@@ -1,85 +1,80 @@
-import './style/formulaire.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import "./style/formulaire.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-var nbform = 1;
-
-function getTd(ligne) {
-  var result =
-    '<div id="' +
-    ligne +
-    '"><label>Nom :</label><input type="text" onChange={(e) => {setNom(e.target.value);}}/><label>Adresse :</label><input type="text" onChange={(e) => {setAdresse(e.target.value);}}/><br />' +
-    '<label>Disponibilité :</label><select name="jour"multiple={true} size="7" onChange={(e) => {setDispo(e.target.value);}}><option value="Lundi">Lundi</option><option value="Mardi">Mardi</option><option value="Mercredi">Mercredi</option><option value="Jeudi">Jeudi</option><option value="Vendredi">Vendredi</option><option value="Samedi">Samedi</option><option value="Dimanche">Dimanche</option></select></div>';
-  return result;
-}
-
-function ajouterform() {
-  var nouvelem = document.createElement('form');
-  nouvelem.innerHTML = getTd(nbform++);
-
-  var dernelem = document.getElementsByTagName('form');
-  var parent = dernelem[dernelem.length - 1];
-  parent.parentNode.insertBefore(nouvelem, parent);
-}
+var tab1 = [];
 
 function Formulaire() {
-  const [nom, setNom] = useState('');
-  const [adresse, setAdresse] = useState('');
-  const [dispo, setDispo] = useState('');
-  const data={nom:nom,adresse:adresse,dispo:dispo};
+  const [nom, setNom] = useState("");
+  const [adresse, setAdresse] = useState("");
+  const [dispo, setDispo] = useState("");
+  const data = { nom: nom, adresse: adresse, dispo: dispo };
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/formulaire', {...data})
-      .then((res) => console.log(res));
+      .get("http://localhost:3000/formulaire", { ...data })
+      .then((res) => console.log(res.data.message));
   }, []);
+  axios
+    .post("http://localhost:3000/formulaire", { ...data })
+    .then((res) => console.log(res.data.message));
 
   function affiche() {
     alert(
-      'Nom : ' + nom + ' Adresse : ' + adresse + ' Disponibilité : ' + dispo
+      "Nom : " + nom + " Adresse : " + adresse + " Disponibilité : " + dispo
     );
   }
 
   return (
-    <div id="formulaire" /*onLoad={init}*/>
+    <div id="formulaire">
       <form action="/log" method="post">
-        <div id="0">
-          <label id='case'>Nom :</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setNom(e.target.value);
-            }}
-          />
-          <label id='case'>Adresse :</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setAdresse(e.target.value);
-            }}
-          />
-          <br />
-          <label id='case'>Disponibilité :</label>
-          <select
-            name="jour"
-            multiple={true}
-            size="7"
-            onChange={(e) => {
-              setDispo(e.target.value);
-            }}
-          >
-            <option value="Lundi">Lundi</option>
-            <option value="Mardi">Mardi</option>
-            <option value="Mercredi">Mercredi</option>
-            <option value="Jeudi">Jeudi</option>
-            <option value="Vendredi">Vendredi</option>
-            <option value="Samedi">Samedi</option>
-            <option value="Dimanche">Dimanche</option>
-          </select>
-        </div>
+        <label id="case">Nom :</label>
+        <input
+          type="text"
+          id="name"
+          onChange={(e) => {
+            setNom(e.target.value);
+          }}
+        />
+        <label id="case">Adresse :</label>
+        <input
+          type="text"
+          id="adr"
+          onChange={(e) => {
+            setAdresse(e.target.value);
+          }}
+        />
+        <br />
+        <label id="case">Disponibilité :</label>
+        <select
+          name="jour"
+          multiple={true}
+          size="7"
+          onChange={(e) => {
+            setDispo(e.target.value);
+          }}
+        >
+          <option value="Lundi">Lundi</option>
+          <option value="Mardi">Mardi</option>
+          <option value="Mercredi">Mercredi</option>
+          <option value="Jeudi">Jeudi</option>
+          <option value="Vendredi">Vendredi</option>
+          <option value="Samedi">Samedi</option>
+          <option value="Dimanche">Dimanche</option>
+        </select>
       </form>
-      <input type="submit" value="Envoyer" id='boutton' onClick={affiche} />
-      <input type="button" value="Ajouter" id='boutton' onClick={ajouterform} />
+      <input type="submit" value="Envoyer" id="boutton" onClick={affiche} />
+      <input
+        type="reset"
+        value="Ajouter"
+        id="boutton"
+        onClick={() => {
+          tab1.push(data);
+          console.table(tab1);
+          document.getElementById("name").value = "";
+          document.getElementById("adr").value = "";
+        }}
+      />
     </div>
   );
 }
