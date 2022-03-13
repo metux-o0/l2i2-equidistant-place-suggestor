dd63225946a6af1ad262413b8e75da859a0ae568
blandinelbb <linblandine@gmail.com>
2022-03-13 19:33:12 +0100

    ajout de style sur la page d'acceuil

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,10 +1,29 @@
-import "./style/home.css";
+import './style/home.css';
+import {
+  Marker,
+  Popup,
+  MapContainer,
+  TileLayer,
+} from 'react-leaflet';
 
 function Home() {
   return (
     <div className="home">
       <h1>Accueil</h1>
-      <p>Slogan</p>
+      <p id="slogan">
+        Profitons de notre jour de repos, qui ne tombe pas à l'eau avec ...
+      </p>
+      <MapContainer
+        id="carte1"
+        center={{ lat: 48.86380957985594, lng: 2.3443822975053807 }}
+        zoom={11}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[48.86380957985594, 2.3443822975053807]}></Marker>
+      </MapContainer>
     </div>
   );
 }
