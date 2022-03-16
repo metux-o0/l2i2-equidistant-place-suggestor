d1bd44cdcca1c41e108898d16098fb08550da388
Boubakar <boubakarbd93@gmail.com>
2022-03-16 18:22:38 +0100

    modification du fichier app.js

--- back/app.js
+++ back/app.js
@@ -1,13 +1,21 @@
-var express = require('express');
+var express = require("express");
+var path = require("path");
 var app = express();
-const path = require('path');
 
-app.get('/a', (req, res) => {
-  console.log('Connecter au localhost:3000 !!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../front/public")));
+
+app.get("/", (req, res) => {
+  console.log("Connecter au localhost:3000 !!");
+});
+app.get("/formulaire", (req, res) => {
+  res.json(req.body);
 });
 
-app.get('/b', (req, res) => {
-  console.log('Bouton !!');
+app.get("/carte", (req, res) => {});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front/public/index.html"));
 });
 
 module.exports = app;
