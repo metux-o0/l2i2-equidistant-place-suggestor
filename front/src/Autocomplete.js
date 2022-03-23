7bb745c6a0e6f2ea7a0da242730ee92073b04010
rafika <rafikaa.sid@gmail.com>
2022-03-23 11:47:19 +0100

    modif map

--- front/src/Autocomplete.js
+++ front/src/Autocomplete.js
@@ -24,7 +24,7 @@ const loadScript = (url, callback) => {
 function handleScriptLoad(updateQuery, autoCompleteRef) {
   autoComplete = new window.google.maps.places.Autocomplete(
     autoCompleteRef.current,
-    { types: ["(cities)"], componentRestrictions: { country: "us" } }
+    { types: ["(cities)"], componentRestrictions: { country: "fr" } }
   );
   autoComplete.setFields(["address_components", "formatted_address"]);
   autoComplete.addListener("place_changed", () =>
