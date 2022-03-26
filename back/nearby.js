d3acda7e10b5d24bb1c135805664fbc40a9f77a9
rafika <rafikaa.sid@gmail.com>
2022-03-26 14:44:36 +0100

    liste de restau

--- back/nearby.js
+++ back/nearby.js
@@ -3,7 +3,7 @@ const { response } = require("express");
 
 var config = {
   method: "get",
-  url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.96203%2C2.53253 &radius=20000&type=restaurant&key=AIzaSyAc_6qQv8VDYWVlUD1qSSas2qPlLW6Ah3U",
+  url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=48.96203%2C2.53253 &radius=20000&type=restaurant&key=KEY",
   headers: {},
 };
 
