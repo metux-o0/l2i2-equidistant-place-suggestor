3684159b0388fe0844e87dc8fbf60768ee9b859a
Boubakar <boubakarbd93@gmail.com>
2022-04-06 14:21:44 +0200

    modification app.js

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import Formulaire from "./Formulaire";
 import Nav from "./Navigateur";
 import Home from "./Home";
 import Header from "./Header";
+import Inscription from "./Inscription";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./Contact";
 
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/formulaire" element={<Formulaire />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/inscription" element={<Inscription />} />
       </Routes>
     </div>
   );
