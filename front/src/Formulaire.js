f82d1c4feb22d75f0143f4dde4e7ccb6b35f5e9f
rafika <rafikaa.sid@gmail.com>
2022-04-09 12:36:40 +0200

    color pin

--- front/src/Formulaire.js
+++ front/src/Formulaire.js
@@ -16,6 +16,17 @@ const containerStyle = {
   height: "500px",
 };
 
+const svgMarker = {
+  path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+  fillColor: "blue",
+  fillOpacity: 0.6,
+  strokeWeight: 0,
+  rotation: 0,
+  scale: 2,
+  //eslint-disable-next-line no-undef
+  anchor: new google.maps.Point(15, 30),
+};
+
 const prochain_jour_numero = () => {
   const date_now = new Date();
   const array = [];
@@ -505,16 +516,30 @@ function Formulaire() {
             );
           })}
           {markers.map((res, index) => {
-            return (
-              <div>
-                <Marker
-                  id="marker"
-                  key={index}
-                  position={res.latlng}
-                  label={res.nom}
-                />
-              </div>
-            );
+            if (index === tab1.length) {
+              return (
+                <div>
+                  <Marker
+                    id="marker"
+                    key={index}
+                    position={res.latlng}
+                    label={res.nom}
+                    icon={svgMarker}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div>
+                  <Marker
+                    id="marker"
+                    key={index}
+                    position={res.latlng}
+                    label={res.nom}
+                  />
+                </div>
+              );
+            }
           })}
         </GoogleMap>
       </div>
