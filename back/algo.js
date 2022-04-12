8ae8303de914115386c01c7a3049c70f16041847
blandinelbb <linblandine@gmail.com>
2022-04-12 15:12:54 +0200

    conflit

--- back/algo.js
+++ back/algo.js
@@ -56,7 +56,12 @@ router.post("/formulaire", async (req, res) => {
               equi = false;
             }
           });
+          axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=restaurant&inputtype=textquery&locationbias=circle%3A2000%4048.86380957985594%2C2.3443822975053807&fields=opening_hours&key=KEY')
+            .then(function (response) {
+              console.log(JSON.stringify(response.data));
+          })
           if (equi) {
+            
             distance_global.push({
               nom: j.nom,
               adresse: j.adresse,
